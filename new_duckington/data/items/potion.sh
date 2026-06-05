918ad6c1fcc16e8de49d012d23093422b63f050a
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-06-05 10:44:04 -0500

    Fixed potion item and removed deprecated scripts for combat options

--- new_duckington/data/items/potion.sh
+++ new_duckington/data/items/potion.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-export GAME_ROOT="$(dirname "$0")/.."
-source "$GAME_ROOT/main.sh"
-source "$GAME_ROOT/system/stats.sh"
 
 ITEM_NAME="Potion"
 ITEM_TYPE="Consumable"
 ITEM_DESC="A mysterious concoction that is made from bear guts and dragon scales. Heals 20 HP."
 item_effect() {
-    if [[ "$PLAYER_HP + 20" -ge "$PLAYER_HP_MAX" ]]; then
+    if [[ "$((PLAYER_HP + 20))" -ge "$PLAYER_HP_MAX" ]]; then
         health_restored=$((PLAYER_HP_MAX - PLAYER_HP))
         PLAYER_HP=$PLAYER_HP_MAX
-        echo "You used $ITEM_NAME, and you restored $health_restored HP. Your HP is now full at $PLAYER_HP/$PLAYER_HP_MAX."
     else
-        PLAYER_HP=$((PLAYER_HP + 20))
-        echo "You used $ITEM_NAME, and Your HP is now at $PLAYER_HP/$PLAYER_HP_MAX."
+        health_restored=20
+        PLAYER_HP=$((PLAYER_HP + $health_restored))
     fi
-
-    # save the game
-    save_player_data
+    echo "You used $ITEM_NAME, and you restored $health_restored HP. Your HP is now full at $PLAYER_HP/$PLAYER_HP_MAX."
 }
 ITEM_RARITY="Common"
 ITEM_NUMBER=003
